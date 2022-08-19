9e30aa6b1fc7a33b96049ef6a5ab5d8fac30d7d9
spencer-nubiral <111589443+spencer-nubiral@users.noreply.github.com>
2022-08-19 11:25:36 -0500

    Update RestServiceApplication.java

--- gs-rest-service-main/initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ gs-rest-service-main/initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceApplication {
-
+//demo push
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
